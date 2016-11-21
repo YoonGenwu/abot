558ffeb62dabb3518193303d980bed4f1408b150
YoonGenwu <jianyu.yin@dena.jp>
2016-11-21 21:56:56 +0800

    update pro

---
+++ Abot_XFormsTest/Abot_XFormsTestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Abot_XFormsTest
+{
+	public partial class Abot_XFormsTestPage : ContentPage
+	{
+		public Abot_XFormsTestPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
